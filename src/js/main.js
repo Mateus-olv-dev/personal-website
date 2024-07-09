272546613361aba31e323e753b9510f05b904bf3
Mateus-olv-dev <mateusolv.dev@gmail.com>
2024-07-09 11:26:32 -0300

    Organizar estrutura do projeto e implementar biblioteca Swiper
    - Reorganizar a estrutura do projeto para melhor modularidade:
      - Mover arquivos JavaScript para src/js/
      - Mover arquivos Sass para src/sass/
      - Mover arquivos específicos de componentes para src/components/
    
    - Implementar a biblioteca Swiper para um novo componente de slider:
      - Instalar o Swiper via npm
      - Adicionar Projetos.js e Projetos.sass em src/components/_projetos/
      - Importar e inicializar o Swiper em Projetos.js
      - Adicionar estilos para o Swiper em Projetos.sass
      - Importar Projetos.sass em src/sass/main.sass usando @use
      - Importar Projetos.js em src/js/main.js

---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+// import '../components/_projetos/_projetos'
+
+
+
+
+
+const copyButton = document.querySelector('.copy_button');
+const textToCopy = document.querySelector('.email');
+const emailAfter = document.querySelector('.email_after')
+const imgButton1 = document.querySelector('.img_button1')
+const imgButton2 = document.querySelector('.img_button2')
+
+
+const nVisivel = () => {
+  textToCopy.style.display = "none"
+}
+
+const visible = () => {
+  emailAfter.style.display = "block"
+}
+
+const trocarImg = () => {
+  imgButton1.style.display = "none"
+  imgButton2.style.display = "block"
+
+ 
+}
+
+
+copyButton.addEventListener('click', () => {
+    const range = document.createRange();
+    range.selectNode(textToCopy);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  //  mudarTxt()
+      nVisivel();
+      visible();
+      trocarImg();
+    ;
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenuIcon.addEventListener('click', () => {
+if (mobileMenu.style.display === 'none' || mobileMenu.style.display === '') {
+  mobileMenu.style.display = 'block';
+} else {
+  mobileMenu.style.display = 'none';
+}
+});
+});
