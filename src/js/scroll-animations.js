4b1c598c3cc87926cb2f08c79d844d0ea70c4389
Mateus-olv-dev <mateusolv.dev@gmail.com>
2024-07-12 16:57:39 -0300

    Implementado animações de scroll

---
+++ src/js/scroll-animations.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll('.hidden')
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+        entry.target.classList.add('show')
+    }else {
+        entry.target.classList.remove('show')
+    }
+  })
+})
+
+elements.forEach( (element) => myObserver.observe(element))
