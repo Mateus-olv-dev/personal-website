3f1ad7849f68df97c8c98f295b6591fd28f9111e
Mateus-olv-dev <mateusolv.dev@gmail.com>
2024-07-13 18:59:12 -0300

    Organização e remoção de código não utilizado

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,47 @@
-
-
 const copyButton = document.querySelector('.copy_button');
 const textToCopy = document.querySelector('.email');
-const emailAfter = document.querySelector('.email_after')
-const imgButton1 = document.querySelector('.img_button1')
-const imgButton2 = document.querySelector('.img_button2')
-
+const emailAfter = document.querySelector('.email_after');
+const imgButton1 = document.querySelector('.img_button1');
+const imgButton2 = document.querySelector('.img_button2');
 
 const nVisivel = () => {
-  textToCopy.style.display = "none"
-}
+  textToCopy.style.display = 'none';
+};
 
 const visible = () => {
-  emailAfter.style.display = "block"
-}
+  emailAfter.style.display = 'block';
+};
 
 const trocarImg = () => {
-  imgButton1.style.display = "none"
-  imgButton2.style.display = "block"
-
- 
-}
-
+  imgButton1.style.display = 'none';
+  imgButton2.style.display = 'block';
+};
 
 copyButton.addEventListener('click', () => {
-    const range = document.createRange();
-    range.selectNode(textToCopy);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
+  const range = document.createRange();
+  range.selectNode(textToCopy);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
   //  mudarTxt()
-      nVisivel();
-      visible();
-      trocarImg();
-    ;
+  nVisivel();
+  visible();
+  trocarImg();
 });
 
 const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 document.addEventListener('DOMContentLoaded', () => {
-
   const menuOpen = document.querySelector('.menu-open');
   const menuClose = document.querySelector('.menu-close');
 
   const toggleMenu = () => {
-    if (mobileMenu.style.display === 'none' || mobileMenu.style.display === '') {
+    if (
+      mobileMenu.style.display === 'none' ||
+      mobileMenu.style.display === ''
+    ) {
       mobileMenu.style.display = 'block';
       menuOpen.style.display = 'none';
       menuClose.style.display = 'block';
@@ -58,50 +53,4 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   mobileMenuIcon.addEventListener('click', toggleMenu);
-
 });
-
-
-// const slides = document.querySelectorAll('.slide');
-// const dots = document.querySelectorAll('.dot');
-// const vejaMaisLinks = ["https://github.com/seu-usuario/projeto1", "https://github.com/seu-usuario/projeto2", "https://github.com/seu-usuario/projeto3"];
-// const vejaMaisBotao = document.querySelector('.veja-mais');
-// let slideAtual = 0;
-
-// function mostrarSlide(n) {
-//     slides.forEach((slide, index) => {
-//         slide.style.display = 'none';
-//         dots[index].classList.remove('active');
-//     });
-//     slides[n].style.display = 'block';
-//     dots[n].classList.add('active');
-// }
-
-// function atualizarLinkVejaMais() {
-//     vejaMaisBotao.setAttribute('href', vejaMaisLinks[slideAtual]);
-// }
-
-// function avancarSlide() {
-//     slideAtual++;
-//     if (slideAtual >= slides.length) {
-//         slideAtual = 0;
-//     }
-//     mostrarSlide(slideAtual);
-//     atualizarLinkVejaMais();
-// }
-
-// function retrocederSlide() {
-//     slideAtual--;
-//     if (slideAtual < 0) {
-//         slideAtual = slides.length - 1;
-//     }
-//     mostrarSlide(slideAtual);
-//     atualizarLinkVejaMais();
-// }
-
-// document.querySelector('.botao-avancar').addEventListener('click', avancarSlide);
-// document.querySelector('.botao-retroceder').addEventListener('click', retrocederSlide);
-
-// mostrarSlide(slideAtual);
-// atualizarLinkVejaMais();
-
--- src/js/scroll-animations.js
+++ src/js/scroll-animations.js
@@ -1,15 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const elements = document.querySelectorAll('.hidden');
-  const headerHeight = document.querySelector('header').offsetHeight; // Altura do header fixo
+  const headerHeight = document.querySelector('header').offsetHeight;
 
   function checkVisibility() {
-    elements.forEach((element) => {
+    elements.forEach(element => {
       const elementTop = element.getBoundingClientRect().top;
       const elementBottom = element.getBoundingClientRect().bottom;
       const windowHeight = window.innerHeight;
 
-      // Verifica se o elemento está pelo menos 10% visível acima e abaixo
-      if ((elementTop - headerHeight <= windowHeight * 0.9) && (elementBottom >= -windowHeight * 0.9)) {
+      if (
+        elementTop - headerHeight <= windowHeight * 0.9 &&
+        elementBottom >= -windowHeight * 0.9
+      ) {
         element.classList.add('show');
       } else {
         element.classList.remove('show');
@@ -17,9 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // Chama a função inicialmente
   checkVisibility();
 
-  // Adiciona um evento de scroll para verificar a visibilidade ao rolar a página
   window.addEventListener('scroll', checkVisibility);
 });
