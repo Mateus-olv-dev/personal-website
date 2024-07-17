7fa239d20ecbdcd9eda8dd873db56aa2885a7b37
Mateus-olv-dev <mateusolv.dev@gmail.com>
2024-07-17 20:16:36 -0300

    Exclusão da biblioteca AOS por apresentar problemas no ambiente mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  AOS.init();
-});
-
 const copyButton = document.querySelector('.copy_button');
 const textToCopy = document.querySelector('.email');
 const emailAfter = document.querySelector('.email_after');
