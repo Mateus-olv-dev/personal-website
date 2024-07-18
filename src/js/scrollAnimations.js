caebd5403cc66c275d756b8d2246e94e0fe92e4e
Mateus-olv-dev <mateusolv.dev@gmail.com>
2024-07-18 12:31:47 -0300

    Implementado animações de scroll com JS vanilla

--- src/js/scrollAnimations.js
+++ src/js/scrollAnimations.js
@@ -17,7 +17,7 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
-  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  const windowTop = window.pageYOffset + (window.innerHeight * 5) / 6;
   target.forEach(function (element) {
     if (windowTop > element.offsetTop) {
       element.classList.add(animationClass);
