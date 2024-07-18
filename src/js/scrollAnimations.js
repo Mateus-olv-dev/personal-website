7a452c83f47a19ff139322ebc4cf520f5bf5356b
Mateus-olv-dev <mateusolv.dev@gmail.com>
2024-07-18 12:33:47 -0300

    Ajuste animações de scroll

--- src/js/scrollAnimations.js
+++ src/js/scrollAnimations.js
@@ -17,7 +17,7 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
-  const windowTop = window.pageYOffset + (window.innerHeight * 5) / 6;
+  const windowTop = window.pageYOffset + (window.innerHeight * 2) / 4;
   target.forEach(function (element) {
     if (windowTop > element.offsetTop) {
       element.classList.add(animationClass);
