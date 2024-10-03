dfd905a4cf91030b690e62093e9fcd50af1955e0
Mateus-olv-dev <mateusolv.dev@gmail.com>
2024-10-03 17:06:45 -0300

    feat: botões de ação no card projetos

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-// src/js/main.js
 import './scrollAnimations';
 import EmblaCarousel from 'embla-carousel';
 import AutoScroll from 'embla-carousel-auto-scroll';
@@ -8,13 +7,10 @@ const viewportNode = emblaNode.querySelector('.embla__viewport');
 
 // Configurações do Embla Carousel e do AutoScroll
 const OPTIONS = { loop: true };
-const AUTO_SCROLL_OPTIONS = { delay: 5000 }; // Ajuste o delay conforme necessário
+const AUTO_SCROLL_OPTIONS = { delay: 5000 };
 
 const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [
   AutoScroll(AUTO_SCROLL_OPTIONS),
 ]);
 
-// Não é necessário adicionar funções para os botões de navegação e controle do autoscroll
-// Se desejar adicionar outros comportamentos, pode fazê-lo aqui
-
-// Adicione event listeners ou outras funcionalidades, se necessário
+// Botão expand skills home
